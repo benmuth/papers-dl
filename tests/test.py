558d7a22a476918a986d4db0f0794260d228a432
Ben Muthalaly <benmuthalaly@gmail.com>
2024-04-13 14:10:18 -0500

    Change basic tests

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,20 @@
-# from src.scihub import SciHub
+from src.scihub import SciHub
 import unittest
-import requests
+# import requests
 
 
 class TestSciHub(unittest.TestCase):
+    def setUp(self):
+        self.scihub = SciHub()
+
     def test_scihub_up(self):
-        url = "https://sci-hub.now.sh/"    
-        response = requests.get(url)
-        self.assertEqual(response.status_code, 200, f"Expected status code 200, but got {response.status_code}")
+        """
+        Tests to verify that `scihub.now.sh` is working
+        """
+        self.assertNotEqual(
+            len(self.scihub.available_base_url_list),
+            0,
+            "Failed to find Sci-Hub domains",
+        )
+        for url in self.scihub.available_base_url_list:
+            self.assertTrue("sci" in url, f"Found URL that may be irrelevant: {url}")
