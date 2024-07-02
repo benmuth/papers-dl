63ea8931f26a12c5ea25af6533a4031205d9d338
Ben Muthalaly <benmuthalaly@gmail.com>
2024-07-02 15:23:39 -0500

    Reorganize test documents

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -9,13 +9,13 @@ target_ids = ("doi", "pmid", "isbn", "issn", "url")
 class TestParser(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.test_material_dir = "tests/identifiers"
+        cls.test_material_dir = "tests/documents"
         cls.valid_id_types = parse.id_patterns.keys()
         for id_type in target_ids:
             if id_type not in cls.valid_id_types:
                 print(f"Skipping testing for {id_type} parsing")
 
-    def test_parse_text(self):
+    def test_parse_text_ids(self):
         "Test to parse identifiers from a set of files."
 
         # NOTE: this test does not fail on false positive matches
@@ -47,11 +47,18 @@ class TestParser(unittest.TestCase):
                     f"ID {expected_id} not found in {file}",
                 )
 
+    def test_parse_text_pdfs(self):
+        for file, expected_url in test_document_links:
+            with open(os.path.join(TestParser.test_material_dir, file), "rt") as f:
+                html_content = f.read()
+                pdf_url = parse.find_pdf_url(html_content)
+            self.assertEqual(pdf_url, expected_url)
+
 
 test_document_ids = {
     "ids.txt": {
         "url": ["https://www.cell.com/current-biology/fulltext/S0960-9822(19)31469-1"],
-        "doi": ["10.1016/j.cub.2019.11.030"],
+        "doi": ["10.1016/j.cub.2019.11.030", "10.1107/s0907444905036693"],
     },
     "bsp-tree.html": {
         "doi": ["10.1109/83.544569"],
@@ -74,3 +81,11 @@ test_document_ids = {
         "isbn": ["9781315365459"],
     },
 }
+
+test_document_links = [
+    (
+        "scidb.html",
+        "https://wbsg8v.xyz/d3/x/1719017408/134/i/scimag/80500000/80542000/10.1016/j.cub.2019.11.030.pdf~/Avtp6y0GwksOGlfLFy9d9Q/Parrots%20Voluntarily%20Help%20Each%20Other%20to%20Obtain%20Food%20Rewards%20--%20Brucks%2C%20D%C3%A9sir%C3%A9e%3B%20von%20Bayern%2C%20Auguste%20M_P_%20--%20Current%20Biology%2C%20%232%2C%2030%2C%20pages%20292-297_e5%2C%20--%2010_1016%2Fj_cub_2019_11_030%20--%20c28dc1242df6f931c29b9cd445a55597%20--%20Anna%E2%80%99s%20Archive.pdf",
+    ),
+    ("scihub.html", "https://sci.bban.top/pdf/10.1016/j.cub.2019.11.030.pdf"),
+]
